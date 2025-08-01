b15aaee33826853c52dc32269129c15f78d38dd5
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-08-01 17:00:01 -0500

    GraphQL test

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 import Image from "next/image";
+import BcGraphQl from "@/components/bc-graphql";
+
+const { BC_STORE_HASH, BC_CHANNEL_ID, BC_GQL_TOKEN, EMAIL, PASSWORD } = process.env;
+
+const token = BC_GQL_TOKEN;
+const endpoint = `https://store-${BC_STORE_HASH}-${BC_CHANNEL_ID}.mybigcommerce.com/graphql`;
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <BcGraphQl token={token ?? ''} endpoint={endpoint ?? ''}
+        email={EMAIL ?? ''} password={PASSWORD ?? ''} />
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
---
+++ components/bc-graphql.tsx
@@ -0,0 +1,83 @@
+'use client';
+
+import { useEffect } from "react";
+
+export default function BcGraphQl({
+  token,
+  endpoint,
+  email,
+  password,
+}: { token: string, endpoint: string, email: string, password: string }) {
+  const logGqlFetch = (gql: string) => {
+    fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Accept-Language': 'en-US',
+      },
+      body: JSON.stringify({ query: gql }),
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log('GraphQL response:', data);
+      })
+      .catch(err => {
+        console.error('GraphQL fetch error:', err);
+      });
+  };
+
+  const login = () => {
+    const gql = `
+      mutation {
+        login(
+          email: "${email}"
+          password: "${password}"
+        ) {
+          customer {
+            email
+          }
+        }
+      }
+    `;
+
+    logGqlFetch(gql);
+  };
+
+  const logout = () => {
+    const gql = `
+      mutation {
+        logout {
+          result
+        }
+      }
+    `;
+
+    logGqlFetch(gql);
+  };
+
+  const getCustomer = () => {
+    const gql = `
+      query {
+        customer {
+          email
+        }
+      }
+    `;
+
+    logGqlFetch(gql);
+  };
+
+  return <>
+    <div>
+      <button onClick={login}>Log In</button>
+    </div>
+    <div>
+      <button onClick={logout}>Log Out</button>
+    </div>
+    <div>
+      <button onClick={getCustomer}>Get Customer</button>
+    </div>
+  </>;
+}
